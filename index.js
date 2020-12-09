192c80923419ee522be09f2b9395bc545fbe5fa6
Nijoane <nicajoane@outlook.com>
2020-12-08 21:23:20 -0300

    adicionando lógica do projeto e layout

---
+++ index.js
@@ -0,0 +1,36 @@
+const elementCard = document.getElementById('container-card');
+const allMovies = 18;
+
+const fetchGhibli = () => {
+  const baseURL = `https://ghibliapi.herokuapp.com/Films/`;
+
+  fetch(baseURL).then(response => {
+    return response.json();
+
+  }).then(dataGhibli => {
+    const movie = dataGhibli.map((result) => ({
+      id: result.id,
+      title: result.title,
+      director: result.director,
+      release_date: result.release_date,
+      description: result.description.substring(0, 450),
+    }));
+    showMovie(movie);
+  });
+}
+fetchGhibli();
+
+const showMovie = (movie) => {
+  console.log(movie)
+  const createMovie = movie.map((movies) => 
+    `<li class="card">
+      <span class='title'>${movies.title}</span>
+      <p class="about">${movies.description}...</p>
+      <img src="images/images.png" class="card-image" alt="totoro-image">
+      <span class="info-movie">Director: ${movies.director} - Year ${movies.release_date}</span>
+    </li>
+    `
+  ).join('');
+  elementCard.innerHTML = createMovie;
+}
+
